3761d6c21fe75b1ec5854f70df106ebed34f08cc
CPTRICODEV <97399891+CPTRICODEV@users.noreply.github.com>
2022-05-17 10:36:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function explodeFunction() {
     document.getElementById('explodetext').style.color = "RED"
-    document.getElementById('explodetext').textContent = "Du har bombet rusland"
+    document.getElementById('explodetext').textContent = "Du har bombet Rusland"
     document.getElementById('explodegif').style.visibility = "visible"
     var audio = new Audio('bomb.mp3');
     audio.play();
